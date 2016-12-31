4b1fc66e507622dc69edabf20d8720bdcc7dc1c8
Sean Harrison <sah@blackearth.us>
2016-12-30 22:26:36 -0600

    no message

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 config = {
   "name": "biblicity_net",
-  "version": "0.2.0",
+  "version": "0.2.1",
   "description": "Python web application for biblicity.net",
   "url": "",
   "author": "Sean Harrison",
