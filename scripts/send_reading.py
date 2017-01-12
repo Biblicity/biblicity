d39e4cba79cd239ded2dba3fe4ce9ebb6307ab13
Sean Harrison <sah@blackearth.us>
2017-01-11 21:32:11 -0600

    .

--- scripts/send_reading.py
+++ scripts/send_reading.py
@@ -11,7 +11,6 @@ c = Dict(os=os, datetime=datetime, config=config, static_url=lambda x:config.Mod
 html = loader.load('email/reading.html').generate(c=c, version='NLT').decode('utf-8')
 
 message = emails.Message(html=html, subject='January 11 Biblicity', mail_from='admin@biblicity.net')
-message.attach(data=open(c.static_url('css/biblicity.css'), 'rb'), filename='biblicity.css')
 message.attach(data=open(c.static_url('img/biblicity_icon.png'),'rb'), filename='biblicity_icon.png', content_disposition='inline')
-message.attach(data=open(c.static_url('css/textapis/NLT.css'),'rb'), filename='NLT.css')
+message.attachments['biblicity_icon.png'].is_inline = True
 message.send(to='sah.harrison@gmail.com')
